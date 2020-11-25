971e16059bbd6886566a1c27936a7d78ccc49298
Terry <terryureche@gmail.com>
2020-11-25 20:20:50 +0200

    -add h2 as database and adapt the crud to work on it

--- src/main/java/com/terry/rest_demo/RestDemoApplication.java
+++ src/main/java/com/terry/rest_demo/RestDemoApplication.java
@@ -2,10 +2,13 @@ package com.terry.rest_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -20,14 +23,20 @@ public class RestDemoApplication {
 
 }
 
+interface CoffeeRepository extends CrudRepository<Coffee, String>{};
+
+@Entity
 class Coffee {
-    private final String id;
+    @Id
+    private String id;
     private String name;
 
     public Coffee(String id, String name) {
         this.id = id;
         this.name = name;
     }
+
+    public Coffee() {}
 
     public Coffee(String name) {
         this(UUID.randomUUID().toString(), name);
@@ -36,6 +45,8 @@ class Coffee {
     public String getId() {
         return id;
     }
+
+    public void setId(String id) { this.id = id; }
 
     public String getName() {
         return name;
@@ -49,10 +60,12 @@ class Coffee {
 @RestController
 @RequestMapping("/coffees") //declar the api for this endpoint
 class RestApiDemoController {
-    private List<Coffee> coffees = new ArrayList<>();
+    private final CoffeeRepository coffeeRepository;
 
-    public RestApiDemoController() {
-        coffees.addAll(List.of(
+    public RestApiDemoController(CoffeeRepository coffeeRepository) {
+        this.coffeeRepository = coffeeRepository;
+
+        this.coffeeRepository.saveAll(List.of(
                 new Coffee("Cafe Cereza"),
                 new Coffee("Cafe Ganador"),
                 new Coffee("Cafe Lareno"),
@@ -65,53 +78,35 @@ class RestApiDemoController {
     //    @GetMapping("/coffees") //the same as above only it's directly a GET type
     @GetMapping //Since we have declared the general url to the class level
     Iterable<Coffee> getCoffees() {
-        return coffees;
+        return coffeeRepository.findAll();
     }
 
     //    @GetMapping("/coffees/{id}")
     @GetMapping("/{id}") //Since we have declared the general url to the class level
     Optional<Coffee> getCoffeeById(@PathVariable String id) {
-        for (Coffee c: coffees) {
-            if(c.getId().equals(id)) {
-                return Optional.of(c);
-            }
-        }
-
-        return Optional.empty();
+        return coffeeRepository.findById(id);
     }
 
     //    @PostMapping("/coffees")
     @PostMapping //Since we have declared the general url to the class level
     Coffee postCoffee(@RequestBody Coffee coffee) {
-        coffees.add(coffee);
-
-        return coffee;
+        return coffeeRepository.save(coffee);
     }
 
     //    @PutMapping("/coffees/{id}")
     //    Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
     @PutMapping("/{id}") //Since we have declared the general url to the class level
     ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-        int coffeeIndex = -1;
-
-        for (Coffee c: coffees) {
-            if(c.getId().equals(id)) {
-                coffeeIndex = coffees.indexOf(c);
-
-                coffees.set(coffeeIndex, coffee);
-            }
-        }
-
         /* return (coffeeIndex == -1) ? postCoffee(coffee) : coffee; */
-        return (coffeeIndex == -1)
-            ? new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) //we will response with status code 201(created)
-            : new ResponseEntity<>(coffee, HttpStatus.OK); //we will response with sattus code 200(ok - updated)
+        return (!coffeeRepository.existsById(id))
+            ? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED) //we will response with status code 201(created)
+            : new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK); //we will response with sattus code 200(ok - updated)
     }
 
     //    @DeleteMapping("/coffees/{id}")
 
     @DeleteMapping("/{id}") //Since we have declared the general url to the class level
     void deleteCoffee(@PathVariable String id) {
-        coffees.removeIf(c -> c.getId().equals(id));
+        coffeeRepository.deleteById(id);
     }
 }
