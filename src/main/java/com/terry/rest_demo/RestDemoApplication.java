e442c18cb5e13eb8f35013ce30ef6f7e43e3b499
Terry <terryureche@gmail.com>
2020-11-25 20:39:54 +0200

    -small refactor

--- src/main/java/com/terry/rest_demo/RestDemoApplication.java
+++ src/main/java/com/terry/rest_demo/RestDemoApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.ArrayList;
@@ -57,6 +59,26 @@ class Coffee {
     }
 }
 
+//@Component
+class DataLoader{
+    private final CoffeeRepository coffeeRepositroy;
+
+    public DataLoader(CoffeeRepository coffeeRepositroy) {
+        this.coffeeRepositroy = coffeeRepositroy;
+    }
+
+    @PostConstruct //@PostContruct restores RestApiDemoController to its intended single purpose of providing an external API and makes the DataLoader responsible for its intended (and obvious) purpose.
+    private void loadData() {
+        this.coffeeRepositroy.saveAll(List.of(
+                new Coffee("Gigel Cereza"),
+                new Coffee("Gigel Ganador"),
+                new Coffee("Gigel Lareno"),
+                new Coffee("Gigel Tres Pontas"),
+                new Coffee("Gigel Zacuscas")
+        ));
+    }
+}
+
 @RestController
 @RequestMapping("/coffees") //declar the api for this endpoint
 class RestApiDemoController {
@@ -65,13 +87,13 @@ class RestApiDemoController {
     public RestApiDemoController(CoffeeRepository coffeeRepository) {
         this.coffeeRepository = coffeeRepository;
 
-        this.coffeeRepository.saveAll(List.of(
-                new Coffee("Cafe Cereza"),
-                new Coffee("Cafe Ganador"),
-                new Coffee("Cafe Lareno"),
-                new Coffee("Cafe Tres Pontas"),
-                new Coffee("Cafe Zacuscas")
-        ));
+//        this.coffeeRepository.saveAll(List.of(
+//                new Coffee("Cafe Cereza"),
+//                new Coffee("Cafe Ganador"),
+//                new Coffee("Cafe Lareno"),
+//                new Coffee("Cafe Tres Pontas"),
+//                new Coffee("Cafe Zacuscas")
+//        ));
     }
 
     //    @RequestMapping(value = "/coffees", method = RequestMethod.GET)
@@ -98,9 +120,9 @@ class RestApiDemoController {
     @PutMapping("/{id}") //Since we have declared the general url to the class level
     ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
         /* return (coffeeIndex == -1) ? postCoffee(coffee) : coffee; */
-        return (!coffeeRepository.existsById(id))
-            ? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED) //we will response with status code 201(created)
-            : new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK); //we will response with sattus code 200(ok - updated)
+        return (coffeeRepository.existsById(id))
+            ? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK) //we will response with sattus code 200(ok - updated)
+            : new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED); //we will response with status code 201(created)
     }
 
     //    @DeleteMapping("/coffees/{id}")
